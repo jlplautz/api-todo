bed8f4dca63b3dd860875259b87a8e5f4078ab7d
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2021-11-15 20:22:27 -0300

    Update tests_main.py

--- api/tests/tests_main.py
+++ api/tests/tests_main.py
@@ -2,7 +2,7 @@ def test_main_status_code_200_ok(client):
     response = client.get('/')
     assert response.status_code == 200
 
-def test_main_return_message():
+def test_main_return_message(client):
     response = client.get('/')
     # assert response.json() == ''
     assert response.json() == {'message': 'Ola, funcionou. Estamos respondendo da raiz da API!!!'}
